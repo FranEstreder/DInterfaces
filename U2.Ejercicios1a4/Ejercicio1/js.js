13b71b20769db57469be129e2163f064d7c4c28a
FranEstreder <franestreder@gmail.com>
2016-10-11 17:03:32 +0200

    Cambios Script

--- U2.Ejercicios1a4/Ejercicio1/js.js
+++ U2.Ejercicios1a4/Ejercicio1/js.js
@@ -1,9 +1,12 @@
+//Esta función obliga a la página a retroceder a la página que estaba anteriormente
 function atras() {
     window.history.back();
 }
+//Esta función obliga a la página a avanzar a la página de la que haya retrocedido
 function delante() {
 	window.history.forward();
 }
+//Esta función obliga a la página a ir a aquella página cuyo número hayamos introducido
 function ir(){
 	var x = prompt("Introduce la página a la que quieres ir (Máximo " + window.history.length + " paginas):");
 	if (!isNaN(x) && isFinite(x))
